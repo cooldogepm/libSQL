da8115d9753ea7336471796a9c351b2fa1c966e8
Doge <cooldogedev@gmail.com>
2023-07-02 17:37:38 +0300

    refactor(SQLException): add default values to constructor

--- src/cooldogedev/libSQL/exception/SQLException.php
+++ src/cooldogedev/libSQL/exception/SQLException.php
@@ -31,12 +31,11 @@ use Exception;
 class SQLException extends Exception
 {
     public function __construct(
-        protected array $_trace,
+        protected array $_trace = [],
 
-        protected string $_traceAsString,
-
-        protected string $_message,
-        protected string $_file,
+        protected string $_traceAsString = "",
+        protected string $_message = "",
+        protected string $_file = "",
 
         protected int $_code = 0,
         protected int $_line = 0,
